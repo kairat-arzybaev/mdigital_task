28689bdf49f5650a3fb3ff3d64b05a6d516fab64
kairat-arzybaev <66504325+kairat-arzybaev@users.noreply.github.com>
2023-09-14 18:30:15 +0600

    Added sorting by alphabet, price and year

--- lib/presentation/pages/car_list_page.dart
+++ lib/presentation/pages/car_list_page.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 import 'package:mdigital_task/presentation/widgets/car_detail_card.dart';
 import '../../domain/models/car.dart';
 import '../../domain/models/car_maker.dart';
+
+enum SortOption {
+  modelAscending,
+  modelDescending,
+  priceAscending,
+  priceDescending,
+  yearAscending,
+  yearDescending,
+}
 
 class CarListPage extends StatefulWidget {
   final CarMaker carMaker;
@@ -16,14 +27,75 @@ class CarListPage extends StatefulWidget {
 
 class _CarListPageState extends State<CarListPage> {
   bool isGridView = false;
+
   int currentPage = 0;
-  int itemsPerPage = 10;
-  int totalPages = 0;
+  int pageSize = 10;
+  List<Car> displayedCars = [];
+
+  SortOption selectedSortOption = SortOption.modelAscending;
+  final ScrollController _listScrollController = ScrollController();
+  final ScrollController _gridScrollController = ScrollController();
 
   @override
   void initState() {
-    totalPages = (widget.carsForMaker.length / itemsPerPage).ceil();
     super.initState();
+    loadMore();
+    _listScrollController.addListener(() {
+      if (_listScrollController.position.pixels ==
+          _listScrollController.position.maxScrollExtent) {
+        loadMore();
+      }
+    });
+    _gridScrollController.addListener(() {
+      if (_gridScrollController.position.pixels ==
+          _gridScrollController.position.maxScrollExtent) {
+        loadMore();
+      }
+    });
+  }
+
+  void sortCars() {
+    setState(() {
+      switch (selectedSortOption) {
+        case SortOption.modelAscending:
+          widget.carsForMaker.sort((a, b) => a.model.compareTo(b.model));
+          break;
+        case SortOption.modelDescending:
+          widget.carsForMaker.sort((a, b) => b.model.compareTo(a.model));
+          break;
+        case SortOption.priceAscending:
+          widget.carsForMaker
+              .sort((a, b) => a.sellingPrice.compareTo(b.sellingPrice));
+          break;
+        case SortOption.priceDescending:
+          widget.carsForMaker
+              .sort((a, b) => b.sellingPrice.compareTo(a.sellingPrice));
+          break;
+        case SortOption.yearAscending:
+          widget.carsForMaker.sort((a, b) => a.year.compareTo(b.year));
+          break;
+        case SortOption.yearDescending:
+          widget.carsForMaker.sort((a, b) => b.year.compareTo(a.year));
+          break;
+      }
+    });
+  }
+
+  void loadMore() {
+    final startIndex = currentPage * pageSize;
+    final endIndex = startIndex + pageSize;
+
+    if (startIndex < widget.carsForMaker.length) {
+      setState(() {
+        displayedCars.addAll(
+          widget.carsForMaker
+              .skip(startIndex)
+              .take(endIndex - startIndex)
+              .toList(),
+        );
+        currentPage++;
+      });
+    }
   }
 
   @override
@@ -41,86 +113,82 @@ class _CarListPageState extends State<CarListPage> {
               });
             },
           ),
+          PopupMenuButton<SortOption>(
+            iconSize: 35,
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                value: SortOption.modelAscending,
+                child: Text('Sort by Model (Ascending)'),
+              ),
+              const PopupMenuItem(
+                value: SortOption.modelDescending,
+                child: Text('Sort by Model (Descending)'),
+              ),
+              const PopupMenuItem(
+                value: SortOption.priceAscending,
+                child: Text('Sort by Price (Ascending)'),
+              ),
+              const PopupMenuItem(
+                value: SortOption.priceDescending,
+                child: Text('Sort by Price (Descending)'),
+              ),
+              const PopupMenuItem(
+                value: SortOption.yearAscending,
+                child: Text('Sort by Year (Ascending)'),
+              ),
+              const PopupMenuItem(
+                value: SortOption.yearDescending,
+                child: Text('Sort by Year (Descending)'),
+              ),
+            ],
+            onSelected: (option) {
+              setState(() {
+                selectedSortOption = option;
+                sortCars();
+              });
+            },
+          ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.arrow_back),
-                  onPressed: () {
-                    if (currentPage > 0) {
-                      setState(() {
-                        currentPage--;
-                      });
-                    }
-                  },
-                ),
-                Text(
-                  'Page ${currentPage + 1} of $totalPages',
-                  style: const TextStyle(fontSize: 16),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.arrow_forward),
-                  onPressed: () {
-                    if (currentPage < totalPages - 1) {
-                      setState(() {
-                        currentPage++;
-                      });
-                    }
-                  },
-                ),
-              ],
+      body: _buildCarList(),
+    );
+  }
+
+  Widget _buildCarList() {
+    if (displayedCars.isEmpty) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: !isGridView
+          ? ListView.builder(
+              controller: _listScrollController,
+              itemCount: displayedCars.length,
+              itemBuilder: (context, index) {
+                final car = displayedCars[index];
+                return CarDetailCard(
+                  model: car.model,
+                  year: car.year,
+                  price: car.sellingPrice,
+                );
+              },
+            )
+          : GridView.builder(
+              controller: _gridScrollController,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: displayedCars.length,
+              itemBuilder: (context, index) {
+                final car = displayedCars[index];
+                return CarDetailCard(
+                  model: car.model,
+                  year: car.year,
+                  price: car.sellingPrice,
+                );
+              },
             ),
-            Expanded(
-              child: widget.carsForMaker.isEmpty
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : isGridView
-                      ? GridView.builder(
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                          ),
-                          itemCount: itemsPerPage,
-                          itemBuilder: (context, index) {
-                            final carIndex = currentPage * itemsPerPage + index;
-                            if (carIndex < widget.carsForMaker.length) {
-                              final car = widget.carsForMaker[carIndex];
-                              return CarDetailCard(
-                                model: car.model,
-                                year: car.year,
-                                price: car.sellingPrice,
-                              );
-                            } else {
-                              return const SizedBox();
-                            }
-                          },
-                        )
-                      : ListView.builder(
-                          itemCount: itemsPerPage,
-                          itemBuilder: (context, index) {
-                            final carIndex = currentPage * itemsPerPage + index;
-                            if (carIndex < widget.carsForMaker.length) {
-                              final car = widget.carsForMaker[carIndex];
-                              return CarDetailCard(
-                                model: car.model,
-                                year: car.year,
-                                price: car.sellingPrice,
-                              );
-                            }
-                            return const SizedBox();
-                          },
-                        ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
--- lib/presentation/widgets/car_detail_card.dart
+++ lib/presentation/widgets/car_detail_card.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-
-import '../../domain/models/car.dart';
 
 class CarDetailCard extends StatelessWidget {
   const CarDetailCard({
